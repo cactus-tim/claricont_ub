f286b7cd474a1d4d80758a1aca0b50d1d70a5ef7
Соснин Тимофей Алексеевич <tasosnin@edu.hse.ru>
2025-02-24 17:35:03 +0300

    prod fix

--- modules/mes_writer.py
+++ modules/mes_writer.py
@@ -3,6 +3,13 @@ import random
 
 from database.req import get_all_targets, update_target
 from handlers.errors import gpt_assystent_mes, create_thread, bots_error_handler
+from instance import client
+
+
+mes = """
+Привет! Видел ты ищешь заказы в чатах в телеграме.\n
+Я недавно тоже начал искать клиентов так, и вот интересно — как у тебя с откликами? Тут реально приходят норм клиенты или в основном молчат?
+"""
 
 
 @bots_error_handler
@@ -18,10 +25,12 @@ async def send_messages(clients, user_id, client_id=0):
                 break
             sent_count = 0
         thread_id = await create_thread()
-        message_text = await gpt_assystent_mes(thread_id)
-        if message_text[-6:-2] == '.docx':
-            message_text = message_text[:-17]
-        await clients[client_id].send_message(target.handler, message_text)
+        init_message = client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="assistant",
+            content=mes
+        )
+        await clients[client_id].send_message(target.handler, mes)
         await update_target(target.handler, {"f_m": True, 'dialog': thread_id})
         sent_count += 1
         await asyncio.sleep(random.randint(180, 720))
